b6e4309a6d4101be1752ffdabc63d8719819961a
And4ik <And4ik11@gmail.com>
2023-03-01 19:23:48 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import './App.css';
 import TodoList from "./TodoList";
 
 // JSX.Element чтобы проследить что в ретурне действительно лежит JSX
+
+// создадим тип описывающий одну таску,это будет обьект( у каждой такски будет id-число, title-название, idDone - cтатус (выполнено или нет)
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 function App(): JSX.Element {
+    // в переменной будет лежать массив элементов типа TaskType. Cоздается набор тасок для одного тудулиста
+    const tasks: TaskType[] = [
+        {id: 1, title:"HTML & CSS", isDone: true},
+        {id: 2, title:"CSS & SCSS", isDone: true},
+        {id: 3, title:"ES6/TS", isDone: false},
+    ]
     return (
         <div className="App">
-            <TodoList title={"What to read"}/>
-            <TodoList title={"What to bue"}/>
-            <TodoList title={"What to learn"}/>
-            <TodoList title={"What"}/>
+            <TodoList title={"What to read"} tasks={tasks}/>
+            <TodoList title={"What to read"} tasks={tasks}/>
+            {/*<TodoList title={"What to bue"}/>*/}
+            {/*<TodoList title={"What to learn"}/>*/}
+            {/*<TodoList title={"What"}/>*/}
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from 'react';
-// описываем тип данных кот будет принмать в качестве параметров функция Todolist
+import {TaskType} from "./App";
+// описываем тип данных который  будет принимать в качестве параметров функция Todolist
 type TodoListPropsType = {
     title: string
+    tasks: Array<TaskType>
 }
 // т.е эта функция будет принимать в качестве параметров обьект, кот будет содержать поле title значением которго явл строка
 // и в качестве параметров компоненты передает обьект (props)
+
+// запихиваем таски в копмоненты TodoList. Но сначала описать в типе Type что эти данные сюда придут
 const TodoList: FC<TodoListPropsType> = (props) => {
     return (
         <div>
@@ -16,9 +20,18 @@ const TodoList: FC<TodoListPropsType> = (props) => {
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                    <li>
+                        <input type="checkbox" checked={props.tasks[0].isDone}/>
+                        {/*из обьекта props берем нулевой элемент массив tasks и берем еще название title*/}
+                        <span>{props.tasks[0].title}</span>
+                    </li>
+                    <li>
+                        <input type="checkbox" checked={props.tasks[1].isDone}/>
+                        <span>{props.tasks[1].title}</span>
+                    </li>
+                    <li><input type="checkbox" checked={props.tasks[2].isDone}/>
+                        <span>{props.tasks[2].title}</span>
+                    </li>
                 </ul>
                 <div>
                     <button>All</button>
